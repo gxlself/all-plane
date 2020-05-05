4eae4fd7f841c8f87e93f73317235998a813823e
Mr_gao <gxlself@gmail.com>
2020-05-05 23:56:22 +0800

    sql

---
+++ 203.sql
@@ -0,0 +1,87 @@
+/*
+SQLyog Professional v12.08 (64 bit)
+MySQL - 5.0.96-community-nt : Database - 203-chat
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`203-chat` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `203-chat`;
+
+/*Table structure for table `m_cache_chat` */
+
+DROP TABLE IF EXISTS `m_cache_chat`;
+
+CREATE TABLE `m_cache_chat` (
+  `from_user` char(16) NOT NULL,
+  `chat_content` longblob,
+  `chat_type` char(20) default NULL,
+  `group_id` int(6) default NULL,
+  `timestamp` datetime default NULL,
+  `chat_content_type` char(20) default NULL,
+  `to_user` char(16) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_cache_chat` */
+
+insert  into `m_cache_chat`(`from_user`,`chat_content`,`chat_type`,`group_id`,`timestamp`,`chat_content_type`,`to_user`) values ('test1','欢迎来到聊天室','groupChat',1,'2020-04-10 00:00:33','text',''),('111111','Nihao ','groupChat',1,'2020-04-10 00:00:40','text',''),('test1','who are you','groupChat',1,'2020-04-10 00:00:49','text',''),('111111','aaaaaa ','groupChat',1,'2020-04-10 00:00:53','text',''),('111111','你好','groupChat',1,'2020-04-10 00:01:00','text',''),('test1','哪个？','groupChat',1,'2020-04-10 00:01:16','text',''),('test1','tell me','groupChat',1,'2020-04-10 00:01:40','text',''),('111111','you know','groupChat',1,'2020-04-10 00:03:58','text',''),('gxlself','嗯','groupChat',1,'2020-04-10 00:58:24','text',''),('1','啥玩意儿','groupChat',1,'2020-04-10 01:05:48','text',''),('1','睡觉','groupChat',1,'2020-04-10 01:05:54','text',''),('gxlself','睡哇','groupChat',1,'2020-04-10 01:06:23','text',''),('1','斗地主','groupChat',1,'2020-04-10 01:06:33','text',''),('gxlself','没人','groupChat',1,'2020-04-10 01:09:17','text',''),('test1','来人啊','groupChat',1,'2020-04-10 12:44:16','text',''),('awy5456','啥玩意','groupChat',1,'2020-04-10 13:03:55','text',''),('17326864148','输入框小了点','groupChat',1,'2020-04-10 13:19:29','text',''),('17326864148','这玩意用户名就是刚刚输入那个啊','groupChat',1,'2020-04-10 13:19:51','text',''),('17326864148','我还以为手机号注册呢哎呀嘛','groupChat',1,'2020-04-10 13:20:00','text',''),('ChineseNoZhiChi','啧啧','groupChat',1,'2020-04-10 14:20:02','text','');
+
+/*Table structure for table `m_group` */
+
+DROP TABLE IF EXISTS `m_group`;
+
+CREATE TABLE `m_group` (
+  `username` char(48) default NULL,
+  `avator` blob,
+  `sex` int(1) default NULL,
+  `group_id` int(6) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_group` */
+
+insert  into `m_group`(`username`,`avator`,`sex`,`group_id`) values ('gxlself','�PNG\r\n\Z\n',1,1),('test1','�PNG\r\n\Z\n',0,1);
+
+/*Table structure for table `m_token` */
+
+DROP TABLE IF EXISTS `m_token`;
+
+CREATE TABLE `m_token` (
+  `token` varchar(43) NOT NULL,
+  `a_token` char(200) NOT NULL,
+  `username` char(16) NOT NULL,
+  PRIMARY KEY  (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_token` */
+
+insert  into `m_token`(`token`,`a_token`,`username`) values ('bFoAMkerCMFROv5RISjIEkH4xmFP3KSORUsPPJqtteY','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEiLCJpYXQiOjE1ODY0NTE5MzUsImV4cCI6MTU4NjYyNDczNX0.bFoAMkerCMFROv5RISjIEkH4xmFP3KSORUsPPJqtteY','1'),('4OzhnKjgiqZZyqHV2uTRPrpdwNcclTj5NZDVjHq5fDM','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjE3MzI2ODY0MTQ4IiwiaWF0IjoxNTg2NDk1OTQ2LCJleHAiOjE1ODY2Njg3NDZ9.4OzhnKjgiqZZyqHV2uTRPrpdwNcclTj5NZDVjHq5fDM','17326864148'),('M6uZ1pmx53V3OI8LbtSNd0H57mnR4XjOBAArzOm5-nE','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjIiLCJpYXQiOjE1ODY0OTYwNTQsImV4cCI6MTU4NjY2ODg1NH0.M6uZ1pmx53V3OI8LbtSNd0H57mnR4XjOBAArzOm5-nE','2'),('iymWDzxCMEExQSM9woPnbh3OorrgADIVeoosOZszZi4','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImF3eTU0NTYiLCJpYXQiOjE1ODY0OTUwMDAsImV4cCI6MTU4NjY2NzgwMH0.iymWDzxCMEExQSM9woPnbh3OorrgADIVeoosOZszZi4','awy5456'),('4atzSeWOQYGO3l1jXCbSTUZ-iBnaWwXa--9yJ9lc1Aw','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkNoaW5lc2VOb1poaUNoaSIsImlhdCI6MTU4NjQ5OTU4OCwiZXhwIjoxNTg2NjcyMzg4fQ.4atzSeWOQYGO3l1jXCbSTUZ-iBnaWwXa--9yJ9lc1Aw','ChineseNoZhiChi'),('1Fzqae1Yo67Klpz2SuWqd-IbPO4CAMt6omsqMJkcFRU','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imd4bHNlbGYiLCJpYXQiOjE1NzYzNzgyNjV9.1Fzqae1Yo67Klpz2SuWqd-IbPO4CAMt6omsqMJkcFRU','gxlself'),('T2FSWTqmM8ZK2s4TLasrGEK658MQlcoVLc1tS6_OwRI','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imxpbmh1YWppZSIsImlhdCI6MTU3NjMzNDU5N30.T2FSWTqmM8ZK2s4TLasrGEK658MQlcoVLc1tS6_OwRI','linhuajie'),('QT19IyXG2ck86accbE8P-4u2xwKBPfvRF7eRUZYqO_8','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxIiwiaWF0IjoxNTg2NDQ3Njg2LCJleHAiOjE1ODY2MjA0ODZ9.QT19IyXG2ck86accbE8P-4u2xwKBPfvRF7eRUZYqO_8','test1'),('1Fto5A37eW3C_haB6jw0qR3b5Qixdx2hbxjfNmTwyR4','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QyIiwiaWF0IjoxNTc2MzM0NTk5fQ.1Fto5A37eW3C_haB6jw0qR3b5Qixdx2hbxjfNmTwyR4','test2'),('Ol5LnmDByn6mEgUa_9d9L09nTKVGQ7GSoLb0TyGfhGU','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3Q0IiwiaWF0IjoxNTg2NDUwMjI5LCJleHAiOjE1ODY2MjMwMjl9.Ol5LnmDByn6mEgUa_9d9L09nTKVGQ7GSoLb0TyGfhGU','test4'),('EWv6ys1XDi8jitJT94bBfB-kApy2BFN8SiMk9F00Ql8','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3Q1IiwiaWF0IjoxNTg2NDUwNDE2LCJleHAiOjE1ODY2MjMyMTZ9.EWv6ys1XDi8jitJT94bBfB-kApy2BFN8SiMk9F00Ql8','test5');
+
+/*Table structure for table `m_users` */
+
+DROP TABLE IF EXISTS `m_users`;
+
+CREATE TABLE `m_users` (
+  `username` char(32) NOT NULL,
+  `password` char(32) NOT NULL,
+  `o_username` char(16) NOT NULL,
+  `o_password` char(16) NOT NULL,
+  `login_status` int(2) default '0',
+  PRIMARY KEY  (`username`,`o_username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_users` */
+
+insert  into `m_users`(`username`,`password`,`o_username`,`o_password`,`login_status`) values ('5677d5a71da3672e37a339d0262fda97','e10adc3949ba59abbe56e057f20f883e','awy5456','123456',1),('5a105e8b9d40e1329780d62ea2265d8a','e10adc3949ba59abbe56e057f20f883e','test1','123456',1),('5c1338f06725fee21bfe6e43ef2a18e0','e10adc3949ba59abbe56e057f20f883e','gxlself','123456',1),('5f8ec14055e33fa2c310c4ffb13ed72e','5f8ec14055e33fa2c310c4ffb13ed72e','linhuajie','linhuajie',1),('6046e3e2e057d84c5e3169254991cf94','4e53b39459e85f528e6cd4f807c69317','awy5456','awy123',1),('67decdaebf0cb8f0f4c7a50c634087ca','980ac217c6b51e7dc41040bec1edfec8','ChineseNoZhiChi','dddddd',1),('68131b63adec1742c73be142dd9effb7','c25986dd5411f576ff28ba7e439b706c','17326864148','herui980322',1),('86985e105f79b95d6bc918fb45ec7727','e10adc3949ba59abbe56e057f20f883e','test4','123456',1),('8ad8757baa8564dc136c1e07507f4a98','e10adc3949ba59abbe56e057f20f883e','test3','123456',1),('96e79218965eb72c92a549dd5a330112','96e79218965eb72c92a549dd5a330112','111111','111111',1),('ad0234829205b9033196ba818f7a872b','e10adc3949ba59abbe56e057f20f883e','test2','123456',1),('c4ca4238a0b923820dcc509a6f75849b','c4ca4238a0b923820dcc509a6f75849b','1','1',1),('c81e728d9d4c2f636f067f89cc14862c','caf1a3dfb505ffed0d024130f58c5cfa','2','321',1),('e3d704f3542b44a621ebed70dc0efe13','e10adc3949ba59abbe56e057f20f883e','test5','123456',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ plane.sql
@@ -0,0 +1,160 @@
+/*
+SQLyog Professional v12.08 (64 bit)
+MySQL - 5.0.96-community-nt : Database - allplant
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`allplant` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `allplant`;
+
+/*Table structure for table `h_token` */
+
+DROP TABLE IF EXISTS `h_token`;
+
+CREATE TABLE `h_token` (
+  `token` char(1) NOT NULL,
+  PRIMARY KEY  (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `h_token` */
+
+/*Table structure for table `h_users` */
+
+DROP TABLE IF EXISTS `h_users`;
+
+CREATE TABLE `h_users` (
+  `name` char(16) collate utf8_unicode_ci NOT NULL,
+  `sex` int(1) default NULL,
+  `address` char(30) collate utf8_unicode_ci default NULL,
+  `mobile` int(11) NOT NULL,
+  `identity_type` int(1) NOT NULL default '1' COMMENT '身份类型：1-游客  2-飞人',
+  `id` varchar(16) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`mobile`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `h_users` */
+
+/*Table structure for table `m_article` */
+
+DROP TABLE IF EXISTS `m_article`;
+
+CREATE TABLE `m_article` (
+  `id` int(8) NOT NULL auto_increment,
+  `title` varchar(50) NOT NULL COMMENT '标题',
+  `theme` varchar(50) NOT NULL COMMENT '主题',
+  `content` longtext COMMENT '内容',
+  `create_user` char(16) default NULL COMMENT '创建者',
+  `create_date` datetime default NULL COMMENT '创建时间',
+  `last_modify` datetime default NULL COMMENT '最近一次更新',
+  `status` int(1) default NULL COMMENT '审核状态',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_article` */
+
+/*Table structure for table `m_article_comment` */
+
+DROP TABLE IF EXISTS `m_article_comment`;
+
+CREATE TABLE `m_article_comment` (
+  `id` int(8) NOT NULL auto_increment COMMENT '评论id',
+  `article_id` int(8) NOT NULL COMMENT '文章id',
+  `remark` varchar(100) NOT NULL COMMENT '评论内容',
+  `images` text COMMENT '图片',
+  `create_date` datetime default NULL COMMENT '创建评论时间',
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `m_article_comment` */
+
+/*Table structure for table `m_menus` */
+
+DROP TABLE IF EXISTS `m_menus`;
+
+CREATE TABLE `m_menus` (
+  `id` int(3) NOT NULL auto_increment COMMENT '菜单id',
+  `menu_name` varchar(60) NOT NULL COMMENT '菜单名',
+  `type` int(1) NOT NULL default '0' COMMENT '类型(0.菜单 1.按钮)',
+  `icon` varchar(30) default NULL COMMENT 'icon',
+  `sort` int(8) default '1' COMMENT '排序',
+  `parent_id` int(3) NOT NULL COMMENT '父节点id',
+  `create_date` datetime default NULL,
+  `url` varchar(60) default NULL COMMENT '页面路径',
+  `enable` tinyint(1) NOT NULL default '1' COMMENT '是否可用',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `m_menus` */
+
+insert  into `m_menus`(`id`,`menu_name`,`type`,`icon`,`sort`,`parent_id`,`create_date`,`url`,`enable`) values (1,'基础信息维护',0,'el-menu',0,0,'2020-04-01 20:29:13','/menulist',1),(2,'菜单管理',0,'el-1',0,1,'2020-04-01 20:29:41','/menu',1),(3,'角色管理',0,'el-role',1,1,'2020-04-01 20:30:02','/role',1),(4,'人员管理',0,'el-people',2,1,'2020-04-01 20:30:26','/people',1);
+
+/*Table structure for table `m_roles` */
+
+DROP TABLE IF EXISTS `m_roles`;
+
+CREATE TABLE `m_roles` (
+  `remark` varchar(30) default NULL,
+  `id` int(3) NOT NULL auto_increment,
+  `role_name` varchar(10) NOT NULL,
+  `menus` text,
+  `enable` tinyint(1) NOT NULL default '1',
+  `last_modify` datetime NOT NULL,
+  `create_date` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `m_roles` */
+
+insert  into `m_roles`(`remark`,`id`,`role_name`,`menus`,`enable`,`last_modify`,`create_date`) values ('这是管理员',2,'管理员','[10,20,15,16,17]',0,'2020-03-11 17:46:47','2020-03-10 17:27:14'),('运营',4,'运营',NULL,0,'2020-03-04 17:46:50','2020-03-04 17:27:14'),('运营1',5,'运营1',NULL,1,'2020-03-06 17:46:52','2020-03-03 17:27:14'),('运营2',6,'运营2',NULL,1,'2020-03-06 17:46:54','2020-03-09 17:27:14'),('123456',7,'测试',NULL,0,'2020-03-31 17:27:14','2020-03-31 17:27:14');
+
+/*Table structure for table `m_token` */
+
+DROP TABLE IF EXISTS `m_token`;
+
+CREATE TABLE `m_token` (
+  `token` varchar(43) NOT NULL,
+  `a_token` char(200) NOT NULL,
+  `username` char(16) NOT NULL,
+  PRIMARY KEY  (`token`,`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_token` */
+
+insert  into `m_token`(`token`,`a_token`,`username`) values ('tySKWst6nhh878e0t13N1RMJJxv3atkzpATx_YHWDLM','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imd4bHNlbGYiLCJpYXQiOjE1ODUyODg3MTN9.tySKWst6nhh878e0t13N1RMJJxv3atkzpATx_YHWDLM','gxlself');
+
+/*Table structure for table `m_users` */
+
+DROP TABLE IF EXISTS `m_users`;
+
+CREATE TABLE `m_users` (
+  `username` char(32) NOT NULL,
+  `password` char(32) NOT NULL,
+  `login_status` int(2) default NULL COMMENT '登录状态',
+  `o_username` char(16) NOT NULL,
+  `o_password` char(16) NOT NULL,
+  `role` text COMMENT '角色',
+  `create_date` datetime default NULL,
+  `last_modify` datetime default NULL,
+  `mobile` int(11) default NULL COMMENT '手机',
+  `avatar` varchar(255) default NULL COMMENT '头像',
+  `introduction` varchar(50) default NULL COMMENT '介绍',
+  PRIMARY KEY  (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `m_users` */
+
+insert  into `m_users`(`username`,`password`,`login_status`,`o_username`,`o_password`,`role`,`create_date`,`last_modify`,`mobile`,`avatar`,`introduction`) values ('5c1338f06725fee21bfe6e43ef2a18e0','e10adc3949ba59abbe56e057f20f883e',NULL,'gxlself','123456',NULL,NULL,NULL,NULL,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
